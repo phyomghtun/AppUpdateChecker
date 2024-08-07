5ee45be1cc40050097ee24591d6ec23e538a0d01
Phyo Maung Htun <phyomaunghtundev@gmail.com>
2024-08-07 17:31:35 +0630

    version 1.0.0-4

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.appupdater.maunghtun"
         minSdk = 24
         targetSdk = 34
-        versionCode = 2
-        versionName = "1.0.1"
+        versionCode = 4
+        versionName = "1.0.4"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
